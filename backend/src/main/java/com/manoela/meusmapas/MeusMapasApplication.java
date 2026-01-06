4bdeba8878f25634b8bf03e554b8093de99c16c0
Manoela Cruz <168127407+manuhcr@users.noreply.github.com>
2026-01-06 11:38:24 -0300

    backend e front separados, agr SIIIM

---
+++ backend/src/main/java/com/manoela/meusmapas/MeusMapasApplication.java
@@ -0,0 +1,11 @@
+package com.manoela.meusmapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusMapasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeusMapasApplication.class, args);
+    }
+}
