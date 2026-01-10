34c04b52e4097d43f31006c056fb24b9658d68d7
Manoela Cruz <168127407+manuhcr@users.noreply.github.com>
2026-01-09 21:03:58 -0300

    finalização parte 1

--- frontend/meusmapasapp/src/components/Mapa.jsx
+++ frontend/meusmapasapp/src/components/Mapa.jsx
@@ -1,21 +1,30 @@
 import {useState} from 'react';
-import {Button, Card, Modal} from 'react-bootstrap'
+import {Button, Card, Modal, Form, Row, Col} from 'react-bootstrap'
 
 
 function Map() {
     const [maps, setMaps] = useState([])
     const [countId, setCountId] = useState(1)
     const [modalShow, setModalShow] = useState(false);
+    const [abrirModal, setAbrirModal] = useState(false);
+    const [nomeMapa, setNomeMapa] = useState(' ');
+    const [mapaAtivo, setMapaAtivo] = useState(null)
 
 
-    function newMap() {
+    function createMap() {
         let newList = []
         let newMap = {
-            id: countId, nomeMapa: 'Mapa 1'
-        }
+            id: countId,
+            nomeMapa: nomeMapa,
+        };
         newList = [...maps, newMap];
         setMaps(newList);
         setCountId(countId + 1)
+        setNomeMapa('');
+
+    }
+
+    function activeMap(achaPorId) {
 
     }
 
@@ -31,6 +40,7 @@ function Map() {
     }
 
     return (<>
+
         <Modal
             show={modalShow}
             onHide={() => setModalShow(false)}
@@ -50,15 +60,53 @@ function Map() {
                 <Button onClick={() => setModalShow(false)}>Não</Button>
             </Modal.Footer>
         </Modal>
-        <Button onClick={newMap} className='botaoCria'>+ Criar novo mapa</Button>
-        <Card> {maps.map(mapa => (<Card key={mapa.id}>
+        <Modal show={abrirModal} onHide={() => setAbrirModal(false)} centered>
+            <Modal.Header>
+                <Modal.Title>Criar novo mapa</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                        <Form.Label column sm={10}>
+                            Nome do mapa:
+                        </Form.Label>
+                        <Col sm={10}>
+                            <Form.Control type="text"
+                                          placeholder="Nome"
+                                          value={nomeMapa}
+                                          onChange={(e) => setNomeMapa(e.target.value)}/>
+                        </Col>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button className='botaoCancel' variant="secondary" onClick={() => setAbrirModal(false)}>
+                    Cancelar
+                </Button>
+
+                <Button className='botaoCreate' variant="primary" onClick={createMap}>
+                    Criar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        <Button onClick={() => setAbrirModal(true)} className='botaoCria'>+ Criar novo mapa</Button>
+
+        {maps.map(mapa => (<Card className="mapas" key={mapa.id}>
             <ul>
-                <li>{mapa.nomeMapa}< /li>
-                <Button onClick={() => excludeMap(mapa.id)}>Excluir</Button>
+                <div>
+                    <li>{mapa.nomeMapa}< /li>
+                    <p><span>0</span> mapas cadastrados</p>
+                </div>
+
+                <div className='botoes'><Button className='botao'>Acessar</Button>
+                    <Button className='botaoExclude' onClick={() => excludeMap(mapa.id)}>Excluir</Button></div>
+
             </ul>
         </Card>))}
-            <Button variant="primary" onClick={() =>
-                setModalShow(true)}>Excluir tudo</Button></Card>
+        <Button className='botaoExcAll' variant="primary" onClick={() =>
+            setModalShow(true)}>Excluir tudo</Button>
 
     </>)
 }
